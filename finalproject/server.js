08c25770e9476c038a38bece90aa7feff6b2b16f
quasimodeaux <gatchey@yahoo.com>
2017-08-03 17:05:02 -0400

    setting up database

--- finalproject/server.js
+++ finalproject/server.js
@@ -1,5 +1,22 @@
 var express = require("express");
-var app = express();
+var app = express()
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').load();
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
+
 
 app.get('/', function(req, res){
     res.send('Welcome to your future');
@@ -9,5 +26,19 @@ app.listen(5000, function(err){
     if (!err)
         console.log("site is live");
     else console.log(err);
+
+});
+
+//Models
+var models = require("./app/models");
+
+//Sync Database
+models.sequelize.sync().then(function() {
+
+    console.log('Nice! Database looks fine')
+
+}).catch(function(err) {
+
+    console.log(err, "Something went wrong with the Database Update!")
 
 });
